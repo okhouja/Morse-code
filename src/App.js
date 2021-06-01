da1a8cbd922faaf31545c522c7b582de855db435
Omar <omar0940@gmail.com>
2021-06-01 11:44:58 +0200

    Morse Code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Morse from "./components/morseCode";
+function App() {
+  // const [converter, setConverter] = Component([]);
+
+  // React Fragments allow you to wrap multiple elements without adding an extra node to the DOM. This can be useful when rendering multiple child elements/components in a single parent component.
+  return (
+    <React.Fragment>
+      <Header />
+      {/* <Morse setConverter={setConverter} /> */}
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Morse Converter</h1>
+    </div>
+  );
+};
+export default Header;
