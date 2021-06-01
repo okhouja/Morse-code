7589e6292f912090b09dd36dc89b555447411ec3
Omar <omar0940@gmail.com>
2021-06-01 16:03:24 +0200

    Morse Code update

--- src/components/Morse.js
+++ src/components/Morse.js
@@ -1,16 +1,29 @@
 import MorseCodes from "./MorseCode";
 import { useState } from "react";
+
 const MorseConverter = () => {
-  const [userInput, setUserInput] = useState("");
-
   console.log(MorseCodes);
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState("");
 
+  const changeHandle = (e) => {
+    setUserInput(e.target.value);
+  };
+  function convertToMorse() {
+    setResult(
+      userInput
+        .toUpperCase()
+        .split("")
+        .map((el) => (MorseCodes[el] ? MorseCodes[el] : el))
+        .join("")
+    );
+  }
   return (
     <div>
-      <textarea rows="5" onChange={} />
-      <div></div>
+      <textarea id="userInput" rows="5" onChange={changeHandle} />
+      <button onClick={convertToMorse}>convert</button>
+      <div>{result}</div>
     </div>
   );
 };
-
 export default MorseConverter;
